cb86b4b9262153a15ac9aa4b7660ebd0d8a0a8ac
Nikki Lydeen <nlydeen@gmu.edu>
2021-07-17 04:54:52 -0600

    Use 95% confidence interval in history plot

--- param_est/4.plot_history.py
+++ param_est/4.plot_history.py
@@ -18,17 +18,19 @@ if __name__ == "__main__":
         hist.append(read_run_specs(i).theta)
 
     param_names = hist[-1].columns
+    n_params = len(param_names)
 
     hist = np.array([x.values for x in hist])
 
-    fig, axes = plt.subplots(hist.shape[2], sharex=True)
+    fig, axes = plt.subplots(n_params, sharex=True)
 
     for i, ax in enumerate(axes):
-        ax.plot(hist[:, :, i].mean(axis=1), color="blue", label="Mean")
+        means = hist[:, :, i].mean(axis=1)
+        half_ci = 1.96 * hist[:, :, i].std(axis=1) / np.sqrt(n_params)
 
-        low, high = np.percentile(hist[:, :, i], [25, 75], axis=1)
-        ax.fill_between(np.arange(n_iter), low, high, color="blue", alpha=0.25,
-                        label="25th/75th %iles")
+        ax.plot(means, color="blue", label="Mean")
+        ax.fill_between(np.arange(n_iter), means - half_ci, means + half_ci,
+                        color="blue", alpha=0.25, label="95% CI")
 
         ax.axhline(REF_VALUES[i], color="red", linestyle="--")
 
